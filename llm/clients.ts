54f8d6a94a1b574cbeac98729be7c85c89f5e61f
Sofoniyastekalegn <126721257+Sofoniyastekalegn@users.noreply.github.com>
2025-11-06 21:48:50 +0300

    add openi client by pioing base url and declare interface  general text loptions modle prompt and instruction temperateur and  llm client async genereate text model pormpt instuctions

---
+++ llm/clients.ts
@@ -0,0 +1,68 @@
+import { InferenceClient, InferenceClient } from "@huggingface/inference";
+
+import { Ollama } from "ollama";
+
+import OpenAI from "openai";
+import summarizePrompt from "../llm/prompts/summarize-reviews.txt";
+
+const OpenAIClient = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+
+
+});
+
+
+const InferenceClient = new InferenceClient(process.env.HF_TOKEN);
+
+const ollamaClient = new Ollama();
+
+
+type GenerateTextOptions = {
+
+    model?: string;
+    prompt: string;
+    instructions?: string;
+    temperature?: number;
+    maxTokens?: number;
+    previousResponseId? string;
+
+
+};
+
+
+type GenerateTextResult = {
+    id: string;
+    text: string;
+};
+
+
+export const llmClient = {
+    async generateText({
+        model = "gpt-4.1",
+        prompt,
+        instructions,
+        temperature = 0.2,
+        maxTokens = 300,
+        previousResponseId, 
+
+    }: GenerateTextOptions): Promise<GenerateTextResult> {
+        const response = await OpenAIClient.responses.create({
+            model,
+            input: prompt,
+            instructions,
+
+            temperature,
+            max_output_tokens: maxTokens,
+            previous_response_id: previousResponseId,
+
+        
+        });
+
+        return {
+            id: response.id,
+            text: response.text,
+
+        };
+    }
+
+}
